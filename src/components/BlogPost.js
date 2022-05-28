efab4f1cd0cae18aa99012401618d8385b032ab5
Isaiah <ijandicos90@gmail.com>
2022-05-27 20:22:34 -0400

    Completed Walkthrough

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,7 +5,7 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration. Btw I completed this walkthrough" />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
